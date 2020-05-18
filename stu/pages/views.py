9b8d299d1de584ac3a22a6441819bc286c32556f
Himanshu <himanshumishra500@gmail.com>
2020-05-18 18:29:58 +0530

    Update views.py

--- stu/pages/views.py
+++ stu/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import TodoList
+from .models import TodoList, status_values, label_values
 
 # Create your views here.
 # def home_view(request,*args,**kwargs):
@@ -12,20 +12,29 @@ from .models import TodoList
 #     return HttpResponse("<h1>Contact us</h1>")
 
 def index(request): 
-    todos = TodoList.objects.all() 
+    todos  = TodoList.objects.all() 
+    labelv = label_values.objects.all()
+    statusv = status_values.objects.all()
     # all the data loaded in todos
 
     if request.method == "POST":
 
         if "taskAdd" in request.POST: 
             
-            title    = request.POST["title"] #title
+            title    = request.POST["description"] #title
             due_date = str(request.POST["date"]) #date
-            label    = request.POST["label"] #category
-            status   = request.POST["status"]
+            label    = request.POST["label_select"] #category
+            status   = request.POST["status_select"]
 
-            Todo = TodoList(title=title, due_date=due_date, label=label, status=status)
-            Todo.save() #saving the todo 
+            Todo = TodoList(title=title, due_date=due_date)
+            Todo.save() #saving the todo
+
+            labelO = label_values(label=label)
+            labelO.save() #saving the todo
+
+            statusO = status_values(status=status)
+            statusO.save() #saving the todo
+            
             return redirect("/") #reloading the page
 
         if "taskDelete" in request.POST:
@@ -35,4 +44,4 @@ def index(request):
                 todo = TodoList.objects.get(title=int(todo_title)) 
                 todo.delete() #deleting todo
 
-    return render(request, "index.html", {"todos": todos})
+    return render(request, "index.html", {"todos": todos, "labelv":labelv, "statusv":statusv})
