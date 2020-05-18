d51a60a6c53dfcf442dcd5101bf88cf4615217f3
Himanshu <himanshumishra500@gmail.com>
2020-05-18 18:30:04 +0530

    Update admin.py

--- stu/pages/admin.py
+++ stu/pages/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 class TodoListAdmin(admin.ModelAdmin):
-    list_display = ("title", "due_date", "label", "status")
+    list_display = ("title", "due_date")
 
 admin.site.register(models.TodoList, TodoListAdmin)
