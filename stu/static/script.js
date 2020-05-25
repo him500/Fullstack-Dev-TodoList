f35911707d08a7dc1a6f923b3edaa585689a30f4
Mukhar Jain <mukhar.jain2009@gmail.com>
2020-05-25 20:49:06 +0530

    added page for archives

--- stu/static/script.js
+++ stu/static/script.js
@@ -1,4 +1,13 @@
+
+function togglearchive(){
+    console.log("AU");
+    document.getElementById("archives").classList.toggle('gold');
+    document.getElementById("archive_display").classList.toggle('active');
+    console.log("AU-pt2");
+    document.getElementById("input_form").classList.toggle('hide');
+}
 function toggleSidebar(){
     console.log("FU");
     document.getElementById("sidebar").classList.toggle('active');
+   
 }
