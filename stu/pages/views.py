3e59f2fea6e2d098219ed7a53b95b37b09c24c33
Himanshu <himanshumishra500@gmail.com>
2020-05-28 19:37:25 +0530

    Update views.py

--- stu/pages/views.py
+++ stu/pages/views.py
@@ -16,7 +16,7 @@ status_json=[
     {'status':'New'},
     {'status':'Completed'}
 ]
-
+todos  = TodoList.objects.all()
 def home(request): 
     todos  = TodoList.objects.all()
     current_date=date.today()
@@ -57,20 +57,47 @@ def home(request):
 
     return render(request, "pages/index.html", {"todos": todos, 'labelv':label_json, 'statusv':status_json})
 
+def index(request):
+    current_date=date.today()
 
+    for i in range(len(todos)):
+        ddate=todos[i].due_date
+        if current_date.year >= ddate.year and current_date.month >= ddate.month and current_date.day > ddate.day:
+            
+            todos[i].archive = True
+            todos[i].save()
+    print("task saved2")
+    if request.method == "POST":
+        print("task saved1")
 
-    # # ------------
+        if "taskAdd" in request.POST: 
+            
+            title    = request.POST["description"] #title
+            due_date = request.POST["date"] #date
+            label    = request.POST["label_select"] #category
+            status   = request.POST["status_select"]
 
-    #     if "taskAdd" in request.POST: #checking if there is a request to add a todo
-    #         title = request.POST["description"] #title
-    #         date = str(request.POST["date"]) #date
-    #         category = request.POST["category_select"] #category
-    #         content = title + " -- " + date + " " + category #content
-    #         Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
-    #         Todo.save() #saving the todo 
-    #         return redirect("/") #reloading the page
-    #     if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-    #         checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-    #         for todo_id in checkedlist:
-    #             todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
-    #             todo.delete() #deleting todo
+            # print("Start",list(map(int, due_date.split("-"))),"end")
+            Todo = TodoList(title=title, due_date=due_date, label=label, status=status)
+            Todo.save() #saving the todo
+            print("task saved")
+
+            return redirect("/") #reloading the page
+
+        if "taskDelete" in request.POST:
+
+            checkedlist = request.POST["checkedbox"] 
+            print(checkedlist,len(checkedlist))
+            todo = TodoList.objects.filter(title=checkedlist) 
+            todo.delete() #deleting todo
+
+            return redirect("/") #reloading the page
+
+    # for todo in TodoList.objects.all():
+    #     print(todo.archive)
+
+    return render(request, "pages/indexN.html", {"todos": todos, 'labelv':label_json, 'statusv':status_json})
+
+def display(request):
+    todos=TodoList.objects.all()
+    return render(request, "pages/display.html", {"todos": todos})
