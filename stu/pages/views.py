c34de5a3663eae4a80a18b167b81e5b109e5d670
Himanshu <himanshumishra500@gmail.com>
2020-05-30 19:22:19 +0530

    Update views.py

--- stu/pages/views.py
+++ stu/pages/views.py
@@ -17,45 +17,6 @@ status_json=[
     {'status':'Completed'}
 ]
 todos  = TodoList.objects.all()
-def home(request): 
-    todos  = TodoList.objects.all()
-    current_date=date.today()
-
-    for i in range(len(todos)):
-        ddate=todos[i].due_date
-        if current_date.year >= ddate.year and current_date.month >= ddate.month and current_date.day > ddate.day:
-            
-            todos[i].archive = True
-            todos[i].save()
-
-    if request.method == "POST":
-
-        if "taskAdd" in request.POST: 
-            
-            title    = request.POST["description"] #title
-            due_date = request.POST["date"] #date
-            label    = request.POST["label_select"] #category
-            status   = request.POST["status_select"]
-
-            # print("Start",list(map(int, due_date.split("-"))),"end")
-            Todo = TodoList(title=title, due_date=due_date, label=label, status=status)
-            Todo.save() #saving the todo
-
-            return redirect("/") #reloading the page
-
-        if "taskDelete" in request.POST:
-
-            checkedlist = request.POST["checkedbox"] 
-            print(checkedlist,len(checkedlist))
-            todo = TodoList.objects.filter(title=checkedlist) 
-            todo.delete() #deleting todo
-
-            return redirect("/") #reloading the page
-
-    # for todo in TodoList.objects.all():
-    #     print(todo.archive)
-
-    return render(request, "pages/index.html", {"todos": todos, 'labelv':label_json, 'statusv':status_json})
 
 def index(request):
     current_date=date.today()
